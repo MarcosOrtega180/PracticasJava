2dd14ea2e559aa9232ff659ea129c5df3bec06ba
maros <marcos.ortega94@gmail.com>
2019-11-11 11:33:16 -0300

    hasta el video 72

--- src/EventosV65/EventosVIEventosVentanaIIIV70.java
+++ src/EventosV65/EventosVIEventosVentanaIIIV70.java
@@ -6,25 +6,26 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowStateListener;
 
 /* si queremos saber cuando una ventana cambió de estado necesitamos utilizar WindowStateListener la cual solo tiene una
-* interfaz, por lo cual no necesitaremos una clase adaptadora, WindowEvent también es interesante debido a que tiene dos
-* métodos, los cuales son getNewState() y getOldState()*/
+ * interfaz, por lo cual no necesitaremos una clase adaptadora, WindowEvent también es interesante debido a que tiene dos
+ * métodos, los cuales son getNewState() y getOldState()*/
 public class EventosVIEventosVentanaIIIV70 {
     public static void main(String[] args) {
         MarcoEstado mimarco = new MarcoEstado();
         mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
-class MarcoEstado extends JFrame{
-    public MarcoEstado(){
+
+class MarcoEstado extends JFrame {
+    public MarcoEstado() {
         setVisible(true);
-        setBounds(300,300,500,350);
+        setBounds(300, 300, 500, 350);
         //aquí ponemos a la escucha a la clase
         addWindowStateListener(new CambioEstado());
     }
 }
 
 //implementamos la interfase winowStateListener en la clase CambioEstado
-class CambioEstado implements WindowStateListener{
+class CambioEstado implements WindowStateListener {
     //esto desencadena una acción cada vez que el estado de la ventana ha sido modificado
     @Override
     public void windowStateChanged(WindowEvent e) {
@@ -32,13 +33,13 @@ class CambioEstado implements WindowStateListener{
         /*para ver el estado actual nosotros podemos consultar directametne las variables de lcase pertenecientes al
         windowEvent
          */
-        System.out.println("estado"+ e.getNewState());
+        System.out.println("estado" + e.getNewState());
         //ahora imprimimos algo cuando la ventana ha sido actualizada
 //        if(e.getNewState()==6){
 //        esto es lo mismo que lo anterior
-        if(e.getNewState()== Frame.MAXIMIZED_BOTH){
+        if (e.getNewState() == Frame.MAXIMIZED_BOTH) {
             System.out.println("La ventana está a pantalla completa.");
-        }else if(e.getNewState()==Frame.NORMAL){
+        } else if (e.getNewState() == Frame.NORMAL) {
             System.out.println("la ventana está normal");
         }
 
---
+++ src/EventosV65/EventosVIIEventosDeTecladoIV71.java
@@ -0,0 +1,47 @@
+package EventosV65;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/*esto se utiliza cuando queremos que nuestro programa ejecute algo cuando se presione un botón especial
+* Para se utiliza la interfaz KeyListener la cuál tiene tres métodos los cuales son keyPressed(KeyEvent e):cuando
+* cualquier tecla es presionada, * keyReleased(KeyEvent e): cuando la tecla ha sido soltada y KeyTyped(KeyEvent e):
+* cuando se han dado los dos métodos anteriores, esta interfaz tiene su adaptador que e KeyAdapter*/
+public class EventosVIIEventosDeTecladoIV71 {
+    public static void main(String[] args) {
+        MarcoConTeclas miMarco= new MarcoConTeclas();
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
+
+class MarcoConTeclas extends JFrame{
+    public MarcoConTeclas(){
+        setVisible(true);
+        setBounds(700,300,600,450);
+        //aquí creamos nuestro listener
+        addKeyListener(new EventoDeTeclado());
+    }
+}
+
+//construimos nuestro objeto keyListener
+class EventoDeTeclado implements KeyListener {
+    public EventoDeTeclado(){
+        System.out.println();
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println("Keytyped: "+ +e.getKeyCode()+" keyChar: "+e.getKeyChar());
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("keyPressed: "+e.getKeyCode()+" keyChar: "+e.getKeyChar());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println("deyReleased: "+e.getKeyCode()+" keyChar: "+e.getKeyChar());
+    }
+}
